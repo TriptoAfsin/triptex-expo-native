50a7fb1d6ada87dea6664e5d7dedf93db148c073
TriptoAfsin <AfsinTripto@gmail.com>
2024-01-20 11:27:10 +0600

    chore: separate front page showcase components

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,7 @@
-import { AlarmCheck, Search } from "@tamagui/lucide-icons";
-import { CheckboxWithLabel } from "components/Checkbox/CheckboxWithLabel";
-import CustomDialog from "components/Dialog/CustomDialog";
-import { InputWithLabel } from "components/Input/InputWithLabel";
-import RadioGroupItemWithLabel from "components/RadioGroup/RadiogroupWithLabel";
-import CustomSelect from "components/Select/CustomSelect";
-import { SwitchWithLabel } from "components/Switch/SwitchWithLabel";
-import { ToastDemo } from "components/Toast/CustomToast";
-import ToggleGroupComponent from "components/ToggleGroup/ToggleGroupComp";
 import { StatusBar } from "expo-status-bar";
+import DummyComponents from "pages/DummyComponents";
 import { StyleSheet, View, useColorScheme } from "react-native";
-import {
-  Button,
-  H1,
-  H2,
-  H3,
-  H4,
-  Input,
-  RadioGroup,
-  ScrollView,
-  Text,
-  TextArea,
-  XStack,
-  YStack,
-} from "tamagui";
+import { ScrollView } from "tamagui";
 
 export default function App() {
   let colorScheme = useColorScheme();
@@ -39,91 +18,7 @@ export default function App() {
     <View style={styles.container}>
       {/* <ToastViewport /> */}
       <ScrollView>
-        <XStack>
-          <YStack mx={30}>
-            <Button bg={"#288dff"} color={"white"} mt={10}>
-              <Text>Button</Text>
-            </Button>
-            <Button mt={10} variant="outlined" outlineColor="#22a7f2">
-              <Text> Button</Text>
-            </Button>
-            <Button
-              alignSelf="center"
-              mt={10}
-              icon={AlarmCheck}
-              bg={"#fd5534"}
-              color={"white"}
-            >
-              <Text color={"white"}>Icon Before</Text>
-            </Button>
-            <Button
-              alignSelf="center"
-              mt={10}
-              iconAfter={Search}
-              bg={"#6650fd"}
-              color={"white"}
-            >
-              <Text color={"white"}>Icon After</Text>
-            </Button>
-            <CustomDialog
-              description={"Dialog description goes here"}
-              title={"Title"}
-              triggerComponent={
-                <Button bg={"#259745"} color={"white"} mt={10}>
-                  <Text>Open Dialog</Text>
-                </Button>
-              }
-              positiveActionComponent={
-                <Button bg={"#037cd5"} color={"white"} bordered>
-                  Tap me
-                </Button>
-              }
-            />
-          </YStack>
-          <YStack mx={30}>
-            <H1>H1</H1>
-            <H2>H2</H2>
-            <H3>H3</H3>
-            <H4>H4</H4>
-            <Text fontSize={16}>Text</Text>
-            <Text fontWeight={"bold"} fontSize={16}>
-              Bold Text
-            </Text>
-          </YStack>
-        </XStack>
-        <YStack mt={20}>
-          <Input size="$4" borderWidth={1} placeholder="Input Element" />
-          <TextArea
-            size="$4"
-            borderWidth={2}
-            mt={10}
-            placeholder="454 South Kafrul, Dhaka"
-          />
-          <InputWithLabel label={"Name"} />
-          <CustomSelect />
-          <CheckboxWithLabel size="$3" label="Accept Terms & Conditions" />
-          <RadioGroup
-            aria-labelledby="Select one item"
-            defaultValue="3"
-            name="form"
-          >
-            <YStack width={300} alignItems="center" space="$2">
-              <RadioGroupItemWithLabel
-                size="$3"
-                value="2"
-                label="Radio Group"
-              />
-            </YStack>
-          </RadioGroup>
-          <SwitchWithLabel size="$2" label="Hello" />
-          <ToggleGroupComponent
-            type="single"
-            size="$3"
-            orientation="horizontal"
-            mt={10}
-          />
-          <ToastDemo />
-        </YStack>
+        <DummyComponents />
       </ScrollView>
       <StatusBar style="auto" />
     </View>
